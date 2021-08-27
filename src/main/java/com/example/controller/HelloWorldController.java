1714b2c1d73444097f8faf89d5a72b033d86f41a
JungWon Park <firstman9@protonmail.com>
2021-08-27 09:20:15 +0900

    탬플릿엔진 추가

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -1,16 +1,21 @@
 package com.example.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
     @GetMapping("/hello2")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
--- src/test/java/com/example/MvcStudyApplicationTests.java
+++ src/test/java/com/example/MvcStudyApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class MvcStudyApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	public void connectionTest() throws Exception{
+		Connection connection = dataSource.getConnection();
+		assertNotNull(connection);
 	}
 
 }
