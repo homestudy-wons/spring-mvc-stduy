b229167e8a66953d2f417e4accc01565b2f154ac
JungWon Park <firstman9@protonmail.com>
2021-08-27 10:04:22 +0900

    탬플릿엔진 추가

--- src/test/java/com/example/MvcStudyApplicationTests.java
+++ src/test/java/com/example/MvcStudyApplicationTests.java
@@ -2,16 +2,17 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MvcStudyApplicationTests {
 
 	@Test
@@ -21,10 +22,18 @@ class MvcStudyApplicationTests {
 	@Autowired
 	DataSource dataSource;
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
 	public void connectionTest() throws Exception{
 		Connection connection = dataSource.getConnection();
 		assertNotNull(connection);
+	}
+	@Test
+	public void testMainPageLoading_ok(){
+		String body = restTemplate.getForObject("/", String.class);
+		assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
 	}
 
 }
