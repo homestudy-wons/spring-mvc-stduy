e72c33be2ea4e7c986dbdaeeea647fd3de31c0c0
kimjiyun <didany15@dongguk.ac.kr>
2021-10-24 12:18:52 +0900

    member entitiy 생성

---
+++ src/main/java/com/example/model/Member.java
@@ -0,0 +1,23 @@
+package com.example.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(length = 20, nullable = false)
+    private String password;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String status;
+
+}
